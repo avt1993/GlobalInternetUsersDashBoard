321e805d20dea1528103f97046405013e6ecb851
Antonio Villardaga <antoniovillardaga@Antonios-Macbook-Pro.local>
2023-05-29 22:07:09 -0700

    Added Sliders to control years

--- Internet_Usage_Dashboard.py
+++ Internet_Usage_Dashboard.py
@@ -2,6 +2,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
+def plot_line_graph (country_selected, y_label_string, title_string, column_to_plot):
+    fig, ax = plt.subplots()
+    ax.plot(country_selected['Year'], country_selected[column_to_plot])
+    fig.tight_layout()
+    ax.fill_between(country_selected['Year'], country_selected[column_to_plot], color = 'lightblue', alpha = 0.5)
+    plt.title(title_string)
+    plt.xlabel('Year')
+    plt.ylabel(y_label_string)
+    plt.grid(True)
+
+    return fig
+
+
+def plot_multi_line_graph (country1_selected, country2_selected, y_label_string, title_string, column_to_plot):
+    fig, ax = plt.subplots()
+    ax.plot(country1_selected['Year'], country1_selected[column_to_plot], label = f" {country1}")
+    ax.plot(country2_selected['Year'], country2_selected[column_to_plot], label = f" {country2}")
+    plt.title(title_string)
+    plt.xlabel('Year')
+    plt.ylabel(y_label_string)
+    plt.grid(True)
+    plt.legend()
+
+    return fig
+
+
+
 st.set_page_config(page_title = "Internet Usage Dashboard",
                    page_icon = ":bar_chart:",
                    layout = "wide"
@@ -39,39 +66,29 @@ if (mode_selected == "Single Country"):
         "Entity == @country"
     )
 
+    start_range = st.sidebar.slider('Start Range', min_value = giu_df['Year'].min(), max_value = giu_df['Year'].max(), value = giu_df['Year'].min())
+    end_range = st.sidebar.slider('End Range', min_value = giu_df['Year'].min(), max_value = giu_df['Year'].max(), value = giu_df['Year'].max())
+
+    country_selected = country_selected[(country_selected['Year'] >= start_range) & (country_selected['Year'] <= end_range)]
+
     country_selected['Increase In Users'] = country_selected['No. of Internet Users'].diff()
     country_selected['Percentage Increase'] = ((country_selected['Increase In Users'] / country_selected['No. of Internet Users'].shift()) * 100).round()
     country_selected = country_selected.drop(["Code", "Unnamed: 0"], axis = 1)
     country_selected.reset_index()
 
-    st.title(f" {country}" + " Dataset")
-    st.dataframe(country_selected, height = 400, width = 1600) 
+
+    st.title("Dataset - " + f" {country}")
+    st.dataframe(country_selected, height = 300, width = 1600) 
+
+
 
     st.title(":bar_chart: " +  f" {country}" + " Line Charts")
-    
+
     # TOTAL INTERNET USERS
-
-    fig1, ax1 = plt.subplots()
-
-    ax1.plot(country_selected['Year'], country_selected['No. of Internet Users'])
-    fig1.tight_layout()
-    ax1.fill_between(country_selected['Year'], country_selected['No. of Internet Users'], color='lightblue', alpha=0.5)
-    plt.title('Total Internet Users from 1980 - 2020')
-    plt.xlabel('Year')
-    plt.ylabel('Total Users')
-    plt.grid(True)
+    fig1 = plot_line_graph(country_selected, 'Total Users', 'Total Internet Users from 1980 - 2020', 'No. of Internet Users')
 
     # PERCENTAGE INCREASE IN INTERNET USERS FROM YEAR TO YEAR
-
-    fig2, ax2 = plt.subplots()
-
-    ax2.plot(country_selected['Year'], country_selected['Percentage Increase'])
-    fig2.tight_layout()
-    ax2.fill_between(country_selected['Year'], country_selected['Percentage Increase'], color='lightblue', alpha=0.5)
-    plt.title('% Increase in Internet Users')
-    plt.xlabel('Year')
-    plt.ylabel('Percentage Increase')
-    plt.grid(True)
+    fig2 = plot_line_graph(country_selected, 'Percentage (%) Increase', 'Increase (%) in Internet Users', 'Percentage Increase')
 
     left_column, right_column = st.columns(2)
     left_column.pyplot(fig1, use_container_width = True)
@@ -95,6 +112,12 @@ else:
     "Entity == @country2"
     )
 
+    start_range = st.sidebar.slider('Start Range', min_value = giu_df['Year'].min(), max_value = giu_df['Year'].max(), value = giu_df['Year'].min())
+    end_range = st.sidebar.slider('End Range', min_value = giu_df['Year'].min(), max_value = giu_df['Year'].max(), value = giu_df['Year'].max())
+
+    country1_selected = country1_selected[(country1_selected['Year'] >= start_range) & (country1_selected['Year'] <= end_range)]
+    country2_selected = country2_selected[(country2_selected['Year'] >= start_range) & (country2_selected['Year'] <= end_range)]
+
     country1_selected['Increase In Users'] = country1_selected['No. of Internet Users'].diff()
     country1_selected['Percentage Increase'] = ((country1_selected['Increase In Users'] / country1_selected['No. of Internet Users'].shift()) * 100).round()
     country1_selected = country1_selected.drop(["Code", "Unnamed: 0"], axis = 1)
@@ -103,32 +126,18 @@ else:
     country2_selected['Percentage Increase'] = ((country2_selected['Increase In Users'] / country2_selected['No. of Internet Users'].shift()) * 100).round()
     country2_selected = country2_selected.drop(["Code", "Unnamed: 0"], axis = 1)    
 
-    st.title(f" {country1}" + " Dataset")
-    st.dataframe(country1_selected, height = 400, width = 1600) 
+    st.title("Dataset - " + f" {country1}")
+    st.dataframe(country1_selected, height = 300, width = 1600) 
 
-    st.title(f" {country2}" + " Dataset")
-    st.dataframe(country2_selected, height = 400, width = 1600) 
+    st.title("Dataset - " + f" {country2}")
+    st.dataframe(country2_selected, height = 300, width = 1600) 
+
 
 
     st.title(":bar_chart:" + f" {country1}" + " VS " + f" {country2}" + " Comparison")
 
-    fig1, ax1 = plt.subplots()
-    ax1.plot(country1_selected['Year'], country1_selected['No. of Internet Users'], label = f" {country1}")
-    ax1.plot(country2_selected['Year'], country2_selected['No. of Internet Users'], label = f" {country2}")
-    plt.title("Total Internet Users from 1980 - 2020")
-    plt.xlabel('Year')
-    plt.ylabel('Internet Users')
-    plt.grid(True)
-    plt.legend()
-
-    fig2, ax2 = plt.subplots()
-    ax2.plot(country1_selected['Year'], country1_selected['Percentage Increase'], label = f" {country1}")
-    ax2.plot(country2_selected['Year'], country2_selected['Percentage Increase'], label = f" {country2}")
-    plt.title("Percentage (%) Change in Internet Usage")
-    plt.xlabel('Year')
-    plt.ylabel("% Increase in Internet Users")
-    plt.grid(True)
-    plt.legend()
+    fig1 = plot_multi_line_graph(country1_selected, country2_selected, 'Internet Users', "Total Internet Users from 1980 - 2020", 'No. of Internet Users')
+    fig2 = plot_multi_line_graph(country1_selected, country2_selected, "% Increase in Internet Users", "Percentage (%) Change in Internet Usage", 'Percentage Increase')
 
     left_column, right_column = st.columns(2)
     left_column.pyplot(fig1, use_container_width = True)

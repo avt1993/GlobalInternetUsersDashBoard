0e6636ac90af69e1e4a9e63a99f528176fbf09c7
Antonio Villardaga <antoniovillardaga@Antonios-Macbook-Pro.local>
2023-05-31 21:20:07 -0700

    Deleted a library

--- Internet_Usage_Dashboard.py
+++ Internet_Usage_Dashboard.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
-import mpldatacursor
+
 
 
 def plot_line_graph(country_selected, y_label_string, title_string, column_to_plot):
